ca734d9b1f8a9173db5562cf4dfe5eb5f6e351b1
xqfang <rnel@rnel-lt19.local>
2024-05-05 20:36:29 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import sys
+from config import DB_DETAILS
+
+
+def main():
+    """
+    Program takes at least one argument
+    :return:
+    """
+    env = sys.argv[1]
+    db_details = DB_DETAILS[env]
+    print(db_details)
+
+
+
+if __name__ == '__main__':
+    main()
